d7c3d8d9af61725e256d5369325d4133b3764086
Pranav Kethireddy <pran.kethi@gmail.com>
2021-06-01 16:03:39 -0700

    some code thats helpful for the backend login
    the logins not done yet

--- js/login.js
+++ js/login.js
@@ -1,14 +1,34 @@
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('.././db/login.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the login database.');
+});
+
+let usernames = [];
+
+db.serialize(() => {
+  db.each(`SELECT * FROM User`, (err, row) => {
     if (err) {
       console.error(err.message);
     }
-    console.log('Connected to the login database.');
+    console.log("ID:" + row.ID + "\nUsername:"+ row.Username + "\nPassword:" + row.Password + "\nAdmin:" + row.Admin);
+    usernames.push(row.Username);
+    console.log(usernames);
   });
+});
+
+var vOneLS = localStorage.getItem("vOneLocalStorage ");
+
+if(usernames.contains(vOneLS)) {
+  var booleanValue = "true";
+  localStorage.setItem("trueORFalse", booleanValue);
+}
 
 db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
--- readme.md
+++ readme.md
@@ -1,16 +1,10 @@
+[![GitHub license](https://img.shields.io/github/license/shriraj20/Play-a-sport)](https://github.com/shriraj20/Play-a-sport/blob/master/LICENSE)
+![GitHub top language](https://img.shields.io/github/languages/top/shriraj20/Play-a-sport)
+
+#Contributors
+- Shriraj Dash
+- Pranav Kethireddy
 # Services
 1. Play with random players
 2. Learn form affiliated companies
 3. Get private lesson from affiliated teachers
-# Work
-## Stage 1
-- Pranav
-  - home page
-- Shriraj
-  - login page, teacher partner page
-## Stage 2
-- tbd
-# Help
-[Help desk](https://newaccount1614656112224.freshdesk.com/support/home)
-# Patrion
-[Donate](https://www.patreon.com/user?u=54510118)
